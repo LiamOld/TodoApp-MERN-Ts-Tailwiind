f0bf9ab1f07453d8e734c0b9def588de6a91972c
Lian Guo <lianguo@Lians-Air.fritz.box>
2024-06-03 17:28:46 +0100

    add adding task function

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,64 @@
+import { ChangeEvent, useState } from "react";
+
+type tasktype = {
+  name: string;
+  done: boolean;
+};
+
 const App = () => {
-  return <div className="bg-red-500">App</div>;
+  const [tasks, setTasks] = useState<tasktype[]>([
+    { name: "first", done: false },
+    { name: "second", done: true },
+    { name: "third", done: true },
+  ]);
+
+  const [task, setTask] = useState<tasktype>({
+    name: "new task",
+    done: false,
+  });
+
+  const handleClick = () => {
+    if (!task.name) {
+      return;
+    }
+    const newtasks = tasks.concat(task);
+    setTasks(newtasks);
+    setTask({ name: "", done: false });
+  };
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const taskname = e.target.value;
+    const newtask = { name: taskname, done: false };
+    setTask(newtask);
+  };
+
+  return (
+    <div className="text-center">
+      <h2 className="text-3xl">Tasks List</h2>
+      <div>
+        <input
+          onChange={handleChange}
+          className="border border-gray-700"
+          type="text"
+          value={task.name}
+        />
+        <button
+          onClick={handleClick}
+          className="bg-gray-300 border border-gray-700 ml-3"
+        >
+          Add
+        </button>
+      </div>
+      <ul>
+        {tasks.map((task) => (
+          <div>
+            <input type="checkbox" checked={task.done}></input>
+            {task.name}
+          </div>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default App;
